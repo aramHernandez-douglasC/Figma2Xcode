feabe0c28a0fb85bef9ec14cf5f449273a55a759
aram hernandez <crypticmorning@outlook.com>
2022-11-07 20:20:57 -0800

    Initial Commit

---
+++ FigmaDesignCode/FigmaDesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FigmaDesignCodeApp.swift
+//  FigmaDesignCode
+//
+//  Created by Aram Hernandez on 2022-11-07.
+//
+
+import SwiftUI
+
+@main
+struct FigmaDesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
